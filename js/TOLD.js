225cbeb0531feae3e6e2a657c81ce2d32940d65e
kurtklink <50058957+kurtklink@users.noreply.github.com>
2019-04-27 21:55:30 -0500

    added wx/told

---
+++ js/TOLD.js
@@ -0,0 +1,16 @@
+"use strict"
+var Excel = require('exceljs');
+var workbook = new Excel.Workbook();
+workbook.xlsx.readFile('bin/TOLD_Calculator_PMP_ver_3.0_2008031.xls')//Change file name here or give file path
+.then(function() {
+    var worksheet = workbook.getWorksheet('sheet');
+    var i=1;
+    worksheet.eachRow({ includeEmpty: false }, function(row, rowNumber) {
+      r=worksheet.getRow(i).values;
+      r1=r[2];// Indexing a column
+      console.log(r1);
+      i++;
+    }); 
+    worksheet.getCell('B3').value = "abc";//Change the cell number here
+return workbook.xlsx.writeFile('file.xlsx')//Change file name here or give     file path
+   });
