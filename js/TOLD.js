da2a25d974b25fd79fd0803399164b920409fe4d
kurtklink <50058957+kurtklink@users.noreply.github.com>
2019-04-28 18:39:11 -0500

    messing w/ js

--- js/TOLD.js
+++ js/TOLD.js
@@ -1,16 +1,14 @@
 "use strict"
-var Excel = require('exceljs');
-var workbook = new Excel.Workbook();
-workbook.xlsx.readFile('bin/TOLD_Calculator_PMP_ver_3.0_2008031.xls')//Change file name here or give file path
-.then(function() {
-    var worksheet = workbook.getWorksheet('sheet');
-    var i=1;
-    worksheet.eachRow({ includeEmpty: false }, function(row, rowNumber) {
-      r=worksheet.getRow(i).values;
-      r1=r[2];// Indexing a column
-      console.log(r1);
-      i++;
-    }); 
-    worksheet.getCell('B3').value = "abc";//Change the cell number here
-return workbook.xlsx.writeFile('file.xlsx')//Change file name here or give     file path
-   });
+function runTOLD{
+    var Excel = require('exceljs');
+    var workbook = new Excel.Workbook();
+    workbook.xlsx.readFile('bin/TestWorkbook.xlsx')//Change file name here or give file path
+    let TempC = document.getElementByID("TempC").value;
+    .then(function() {
+        var worksheet = workbook.getWorksheet('TOLDin');
+        worksheet.getCell('E7').value = TempC;//Change the cell number here
+    return workbook.xlsx.writeFile('TestWorkbook.xlsx')//Change file name here or give     file path
+        });
+}
+
+
