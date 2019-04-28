b3f1d19c35ab81c4bc74fe43f93a42f2c656f884
kurtklink <50058957+kurtklink@users.noreply.github.com>
2019-04-28 18:51:55 -0500

    Update wx.js

--- js/wx.js
+++ js/wx.js
@@ -1,41 +1,17 @@
 "use strict"
 function getWX() {
 	let StationID = document.getElementById("StationID").value;
-	// const ADDS = require('adds')
-	// const view = value => console.log(JSON.stringify(value, null, 2))
 	document.getElementById("DTGfetched").innerHTML = "Data fetched at: " + new Date();
 	document.getElementById("testOutput").innerHTML = StationID;
-	
-	// Promise.all([
-	// ADDS('metars', {
-	// 	stationString: StationID,
-	// 	hoursBeforeNow: 1,
-	// 	mostRecentForEachStation: true
-	// }),
-	// // ADDS('tafs', {
-	// // 	stationString: 'KJFK',
-	// // 	hoursBeforeNow: 1,
-	// // 	mostRecentForEachStation: true
-	// // }),
-	// // ADDS('aircraftreports', {
-	// // 	hoursBeforeNow: 1,
-	// // 	mostRecent: true
-	// // }),
-	// // ADDS('airsigmets', {
-	// // 	hoursBeforeNow: 6,
-	// // 	mostRecent: true
-	// // }),
-	// // ADDS('gairmets', {
-	// // 	hoursBeforeNow: 6,
-	// // 	mostRecent: true
-	// // }),
-	// // ADDS('stations', {
-	// // 	stationString: 'PANC'
-	// // })
-	// ])
-	// .then(results => {
-	// 	view(results)
-	// })
+	const ADDS = require('adds')
+
+	ADDS('metars', {
+	  stationString: 'KSEA',
+	  hoursBeforeNow: 1
+	})
+	  .then(metars => {
+	    console.log(JSON.stringify(metars, null, 2))
+	  })
 }
 
 
